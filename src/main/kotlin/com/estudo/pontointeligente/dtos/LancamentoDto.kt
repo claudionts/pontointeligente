d616ccad4b5c10378c36b685f299013c3abb2c59
Claudio Neto <jneto@mcontigo.com>
2018-07-17 16:53:47 -0300

    Adicionando DTO's

--- src/main/kotlin/com/estudo/pontointeligente/dtos/LancamentoDto.kt
+++ src/main/kotlin/com/estudo/pontointeligente/dtos/LancamentoDto.kt
@@ -1,4 +1,17 @@
 package com.estudo.pontointeligente.dtos
 
-class LancamentoDto {
-}
+import org.hibernate.validator.constraints.NotEmpty
+
+data class LancamentoDTO (
+        @get:NotEmpty(message = "Data não pode ser vazia")
+        val data: String? = null,
+
+        @get:NotEmpty(message = "Tipo não pode ser vazio")
+        val tipo: String? = null,
+        val funcionarioId:String? = null,
+
+        val descricao:String? = null,
+        val localizacao:String? = null,
+
+        var id:String? = null
+)
